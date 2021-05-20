f2e2ddf7c99128f6d3d9b1f00755c49a938ff2fa
Khoby-790 <ebaidoo79@gmail.com>
2021-05-20 12:38:36 +0000

    --- Auto-Git Commit ---

--- src/components/mobile/BottomTabNavigation.jsx
+++ src/components/mobile/BottomTabNavigation.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import MobileTapNav from "./mobileNavTap";
 
 const BottomTabNavigation = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <MobileTapNav>
+      <NavItem
+        icon={<BsFillGrid1X2Fill size={24} />}
+        title={"Home"}
+        active={currentScreen === 0 ? true : false}
+        index={0}
+      />
+      <NavItem
+        icon={<AiFillHdd size={24} />}
+        title={"Categories"}
+        active={currentScreen === 1 ? true : false}
+        index={1}
+      />
+      <NavItem
+        icon={<BsFillGridFill size={24} />}
+        title={"Explore"}
+        active={currentScreen === 2 ? true : false}
+        index={2}
+      />
+      <NavItem
+        icon={<BsFillTagFill size={24} />}
+        title={"Promos"}
+        active={currentScreen === 3 ? true : false}
+        index={3}
+      />
+      <NavItem
+        icon={<FaHeadphonesAlt size={24} />}
+        title={"Help"}
+        active={currentScreen === 4 ? true : false}
+        index={4}
+      />
+    </MobileTapNav>
+  );
+};
 
-export default BottomTabNavigation
+export default BottomTabNavigation;
