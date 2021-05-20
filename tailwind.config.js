55a5a010e38c5efc3d647f9d3c5c6293358fd750
Khoby-790 <ebaidoo79@gmail.com>
2021-05-20 12:54:07 +0000

    --- Auto-Git Commit ---

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "tendo-bg": "rgb(21, 24, 30)"
+        "tendo-bg": "rgb(21, 24, 30)",
+        "tendo-active": "rgb(33, 150, 243)"
       }
     },
   },
