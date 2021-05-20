3331eda966322dbf72a421c8be3aded47c4749ed
Khoby-790 <ebaidoo79@gmail.com>
2021-05-20 12:41:01 +0000

    --- Auto-Git Commit ---

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "tendo-bg": "rgb(21, 24, 30)"
+      }
+    },
   },
   variants: {
     extend: {},
