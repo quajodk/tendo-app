95c2bd515a0be4e98a5c7f625e780e5a4fb1e27c
Khoby-790 <ebaidoo79@gmail.com>
2021-05-20 12:51:56 +0000

    --- Auto-Git Commit ---

--- src/components/mobile/Header.jsx
+++ src/components/mobile/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = ({ title }) => {
   const screens = useSelector((state) => state.mobileScreens);
@@ -7,7 +8,9 @@ const Header = ({ title }) => {
   return (
     <div className="bg-tendo-bg py-3">
       <div className="flex w-screen ">
-        <div className="w-20 "></div>
+        <div className="w-20 flex justify-center items-center ">
+          <AiOutlineMenu color="white" />
+        </div>
         <div className="flex-1 text-center">
           <span className="text-white font-medium text-xl">
             {screens[currentScreen]["title"]}
