fd3c8fd15a50dbf413884732cad7c81b9cd24d70
Khoby-790 <ebaidoo79@gmail.com>
2021-05-21 07:40:45 +0000

    --- Auto-Git Commit ---

--- src/layout/mobile/MobileLayout.jsx
+++ src/layout/mobile/MobileLayout.jsx
@@ -1,26 +1,10 @@
-import React, { Fragment } from "react";
-import AppBar from "../../components/mobile/appBar";
-import MobileBody from "../../components/mobile/mobileBody";
-import MobileTapNav from "../../components/mobile/mobileNavTap";
+import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
-import NavItem from "../../components/mobile/navItem";
-import ProductDetailsBody from "../../components/mobile/mobileProductDetail";
-import ProductAppBar from "../../components/mobile/detailsAppBar";
-import OrderForm from "../../components/mobile/orderForm";
-import CategoryProductsScreen from "../../components/mobile/categoriesProducts";
 import BottomTabNavigation from "../../components/mobile/BottomTabNavigation";
 import Header from "../../components/mobile/Header";
-import ProductListing from "./ProductListing";
-import HomeTab from "./tabs/HomeTab";
+
 import { routes } from "./routes";
 const MobileLayer = () => {
-  const screens = useSelector((state) => state.mobileScreens);
-  const currentScreen = useSelector((state) => state.currentMobileScreen);
-  const selectedMobileItem = useSelector((state) => state.mobileProductSelect);
-  const productName = useSelector((state) => state.productName);
-  const showOrderForm = useSelector((state) => state.showOrderForm);
-  const categorySelected = useSelector((state) => state.categorySelected);
   return (
     <div className="flex h-screen flex-1  flex-col">
       {/* Handle when a product is selected */}
