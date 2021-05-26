050f944bd7c3dbf271c270d390d616e82a07295b
Khoby-790 <ebaidoo79@gmail.com>
2021-05-26 16:08:15 +0000

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import Shop from "./layout/Shop";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./redux/store";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./graphql/client";
 import { BrowserRouter } from "react-router-dom";
@@ -11,10 +12,12 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <BrowserRouter>
-          <ScrollToTop />
-          <Shop />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Shop />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ApolloProvider>
   );
