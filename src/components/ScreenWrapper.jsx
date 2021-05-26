c54b202ede4b076f948ea0d911691b34b94a8c63
Khoby-790 <ebaidoo79@gmail.com>
2021-05-26 16:25:43 +0000

    --- Auto-Git Commit ---

--- src/components/ScreenWrapper.jsx
+++ src/components/ScreenWrapper.jsx
@@ -11,14 +11,14 @@ const ScreenWrapper = ({
 }) => {
   return (
     <div className="h-screen flex-1 flex flex-col">
+      {/* Header component */}
+      <Header
+        goBack={backFunction}
+        search={searchFunction}
+        title={title}
+        showBack={showBackBtn}
+      />
       <div className="h-auto flex-1 lg:py-6 overflow-y-scroll pb-3 bg-tendo-bg">
-        {/* Header component */}
-        <Header
-          goBack={backFunction}
-          search={searchFunction}
-          title={title}
-          showBack={showBackBtn}
-        />
         <div className="flex-1 h-full ">{children}</div>
       </div>
       <BottomTabNavigation />
