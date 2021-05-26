e17fc2ce061fb7f54af85a975338eb8cb84794be
Khoby-790 <ebaidoo79@gmail.com>
2021-05-26 13:55:24 +0000

    Changes made to the system

--- src/components/ScreenWrapper.jsx
+++ src/components/ScreenWrapper.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Header from "./Header";
 
-const ScreenWrapper = ({ title, showBackBtn, searchFunction, children }) => {
+const ScreenWrapper = ({
+  title,
+  showBackBtn,
+  searchFunction,
+  children,
+  backFunction,
+}) => {
   return (
     <div className="h-screen flex-1 flex flex-col">
       {/* Header component */}
-      <Header search={searchFunction} title={title} showBack={showBackBtn} />
+      <Header
+        goBack={backFunction}
+        search={searchFunction}
+        title={title}
+        showBack={showBackBtn}
+      />
       <div className="flex-1 overflow-y-scroll bg-tendo-bg">{children}</div>
     </div>
   );
