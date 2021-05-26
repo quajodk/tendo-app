03f16446911f08a17560ed917d77bdffdfe651d2
Khoby-790 <ebaidoo79@gmail.com>
2021-05-26 14:00:24 +0000

    Changes made to the users

--- src/layout/ShopContent.jsx
+++ src/layout/ShopContent.jsx
@@ -1,24 +1,24 @@
-import React, { Fragment } from 'react'
-import ItemsListing from '../components/ItemsListing'
-import FlitersPane from './FlitersPane'
-import SelectedItem from '../components/SelectedItem'
-import Cart from '../components/Cart'
+import React, { Fragment } from "react";
+import ItemsListing from "../components/ItemsListing";
+import FlitersPane from "./FlitersPane";
+import SelectedItem from "../components/SelectedItem";
+import Cart from "../components/Cart";
+import ScreenWrapper from "../components/ScreenWrapper";
 
 const ShopContent = () => {
-    return (
-        <Fragment>
-            <div className="flex-grow w-full lg:flex">
-                {/* <!-- Left sidebar & main wrapper --> */}
-                <div className="flex-1 min-w-0 bg-white dark:bg-gray-800 xl:flex">
-                    <FlitersPane />
-                    <ItemsListing />
-                </div>
-                <SelectedItem />
-                <Cart />
-            </div>
+  return (
+    <ScreenWrapper title="">
+      <div className="flex-grow w-full lg:flex">
+        {/* <!-- Left sidebar & main wrapper --> */}
+        <div className="flex-1 min-w-0 bg-white dark:bg-gray-800 xl:flex">
+          <FlitersPane />
+          <ItemsListing />
+        </div>
+        <SelectedItem />
+        <Cart />
+      </div>
+    </ScreenWrapper>
+  );
+};
 
-        </Fragment>
-    )
-}
-
-export default ShopContent
+export default ShopContent;
