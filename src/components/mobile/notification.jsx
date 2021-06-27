774b58997abc00c428fe8f8146c319ea165f3d8e
Quajo Duke <quajoduke305@gmail.com>
2021-06-27 17:47:35 +0000

    added notification page

---
+++ src/components/mobile/notification.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ScreenWrapper from "../ScreenWrapper";
+import { ReactComponent as AnimatedSvg } from "../../assets/push-notifications-animate,svg";
+
+const Notifications = () => {
+  return (
+    <ScreenWrapper showBackBtn>
+      <div className="flex flex-col justify-center items-center w-full h-screen">
+        <AnimatedSvg className="w-80 h-80 my-6" />
+        <div className="onesignal-customlink-container"></div>
+      </div>
+    </ScreenWrapper>
+  );
+};
+
+return Notifications;
