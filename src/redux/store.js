7a5771a9ce963e02028e0d3b057a93b13c6ea62f
Quajo Duke <quajoduke305@gmail.com>
2021-07-28 14:16:44 +0000

    category images

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import reducer from "./reducers/shopReducer";
+import autoMergeLevel1 from "redux-persist/lib/stateReconciler/autoMergeLevel1";
 
 const persistConfig = {
   key: "root",
   storage,
   blacklist: ["auth", "currentMobileScreen"],
+  stateReconciler: autoMergeLevel1,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
