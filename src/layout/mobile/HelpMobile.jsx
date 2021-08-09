83c8e72b9909d3be6c747e9df58685970ac9b4bb
Quajo Duke <quajoduke305@gmail.com>
2021-08-09 16:05:43 +0000

    added sign out

--- src/layout/mobile/HelpMobile.jsx
+++ src/layout/mobile/HelpMobile.jsx
@@ -10,12 +10,16 @@ import {
 } from "react-icons/hi";
 import { BsFillPersonFill } from "react-icons/bs";
 import { FiPackage } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import ScreenWrapper from "../../components/ScreenWrapper";
+import { Dialog, Transition } from "@headlessui/react";
+import { HiOutlineX } from "react-icons/hi";
 
 const HelpMobile = () => {
+  let [isPaymentOpen, setIsPaymentOpen] = React.useState(false);
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const login = () => {
     dispatch({
@@ -38,6 +42,25 @@ const HelpMobile = () => {
   //   }, a reseller on TendoNg 🇳🇬 App. I require some assistance`;
   //   window.open(`https://wa.me/+2349014992643/?text=${message}`, "blank");
   // };
+
+  const signOut = () => {
+    dispatch({
+      type: "authenticateUser",
+      payload: null,
+    });
+    localStorage.removeItem("resellerToken");
+    history.push("/");
+  };
+
+  function closePaymentModal() {
+    setIsPaymentOpen(false);
+  }
+
+  function closeModalSignOut() {
+    setIsPaymentOpen(false);
+    signOut();
+  }
+
   return (
     <ScreenWrapper title="Account">
       <div className="flex lg:justify-center justify-start w-full">
@@ -111,9 +134,80 @@ const HelpMobile = () => {
             <div className="flex items-center text-lg text-tendo-active my-4">
               <HiOutlineInformationCircle size={25} className="mr-6" /> About
             </div>
+            <div
+              className="flex items-center text-lg text-tendo-active my-4 cursor-pointer"
+              onClick={() => setIsPaymentOpen(true)}
+            >
+              <HiOutlineInformationCircle size={25} className="mr-6" /> Sign Out
+            </div>
           </div>
         </div>
       </div>
+
+      <Transition appear show={isPaymentOpen} as={React.Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closePaymentModal}
+          open={isPaymentOpen}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={React.Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={React.Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Are sure you want sign out?
+                  </p>
+                </div>
+
+                <div className="mt-4 flex justify-between">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closePaymentModal}
+                  >
+                    No, keep me signed in
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-gray-100 border border-transparent rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModalSignOut}
+                  >
+                    Yes, sign me out
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </ScreenWrapper>
   );
 };
